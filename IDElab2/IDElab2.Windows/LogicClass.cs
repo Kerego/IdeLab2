08fbe6b049579b820a82a5d43603b8cfcbb9ab0a
Kerego <kerego.aka.nillan@gmail.com>
2015-04-05 17:51:30 +0300

    Extract Method to Logic class

--- IDElab2/IDElab2.Windows/LogicClass.cs
+++ IDElab2/IDElab2.Windows/LogicClass.cs
@@ -14,6 +14,7 @@ namespace IDElab2
     public class LogicClass
     {
 
+        static Random random = new Random();
         public static bool checkShipStatus(ref ShipStatus shipStatus)
         {
             if (shipStatus.battleLog.Count > 10)
@@ -21,12 +22,13 @@ namespace IDElab2
                     shipStatus.battleLog.RemoveAt(i);
 
             if (shipStatus.health > 0)
-                checkForDamage(ref shipStatus);
+            {
+                if (checkForDamage(ref shipStatus))
+                    checkForRepair(ref shipStatus);
+            }
             else
                 return false;
 
-            if (shipStatus.health < 100)
-                checkForRepair(ref shipStatus);
 
             if (shipStatus.fuel > 0)
             {
@@ -40,26 +42,23 @@ namespace IDElab2
             return true;
         }
 
-        public static void checkForDamage(ref ShipStatus shipStatus)
+        public static bool checkForDamage(ref ShipStatus shipStatus)
         {
-            Random random = new Random();
-            if (random.Next(100) < 81)
+            if (random.Next(100) < 60)
             {
                 var damage = (100 - shipStatus.sliderValue) / 10 + random.Next(5) - (shipStatus.useShields ? (shipStatus.useHighPowerShields ? 4 : 0) + (shipStatus.useLowPowerShields ? 2 : 0) : 0);
                 shipStatus.health -= damage;
                 shipStatus.battleLog.Insert(0, new TextBlock() { Text = "An enemy ship attacked our ship. -" + damage + "HP", Foreground = new SolidColorBrush(Colors.Red), TextAlignment = TextAlignment.Center, Width = 488 });
+                return false;
             }
+            return true;
         }
 
         public static void checkForRepair(ref ShipStatus shipStatus)
         {
-            Random random = new Random();
-            if (random.Next(100) < 51)
-            {
                 var damage = shipStatus.sliderValue / 10 + random.Next(5);
                 shipStatus.health += damage;
                 shipStatus.battleLog.Insert(0, new TextBlock() { Text = "Our mechanics managed to repair the ship. +" + damage + "HP", Foreground = new SolidColorBrush(Colors.Green), TextAlignment = TextAlignment.Center, Width = 488 });
-            }
         }
     }
 }
